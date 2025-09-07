bf39f92440175d470aa4b80966314f7d058956a8
Fiorella <fiorelladv13@gmail.com>
2025-09-07 15:46:43 -0400

    Ultima version

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Challenge-Amigo-Secreto
+🎉 Challenge-Amigo-Secreto
+
+El Sorteador de Amigos es una aplicación web sencilla creada para practicar lógica de programación con JavaScript. Permite agregar nombres de amigos, mostrarlos en una lista dinámica y seleccionar aleatoriamente a uno de ellos. Ideal para decisiones rápidas entre amigos o juegos divertidos.
+
+🌟 Funcionalidades
+
+Agregar amigos: Introduce un nombre y añádelo a la lista.
+
+Lista dinámica: Cada vez que agregas un amigo, la lista se actualiza automáticamente.
+
+Sortear un amigo: Selecciona un amigo al azar y muéstralo en pantalla.
+
+💻 Tecnologías
+
+HTML para la estructura de la página.
+
+JavaScript para la lógica de agregar, mostrar y sortear amigos.
+
+CSS (opcional) para estilizar la lista y botones.
+
+📝 Uso
+
+Abrir el archivo index.html en tu navegador.
+
+Escribir un nombre en el campo de texto y presionar Añadir.
+
+Ver cómo el nombre se agrega a la lista de amigos.
+
+Presionar Sortear para seleccionar un amigo al azar.
--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    const nombre = document.getElementById("amigo").value;
+    if (nombre !== ""){
+        amigos.push(nombre);
+        document.getElementById("amigo").value = '';
+    }
+    else {
+        alert("Por favor, inserte un nombre.");
+    }
+
+    actualizarLista();
+}
+
+function actualizarLista(){
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    // Actualiza la lista de amigos mostrandola en la pantalla
+    for (let amigo of amigos){
+        const nuevoLi = document.createElement("li");
+        nuevoLi.innerHTML = amigo;
+        lista.appendChild(nuevoLi);
+    }    
+}
+
+function sortearAmigo(){
+    if (amigos.length > 0){
+        const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        //Accede al nombre de un amigo de manera aleatoria usando el índice
+        const nombreAleatorio = amigos[indiceAleatorio];
+        document.getElementById("resultado").innerHTML = nombreAleatorio;
+    }
+    else{
+        alert("No hay amigos para sortear.");
+    }
+    
+}
